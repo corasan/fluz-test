dca2f48a34c2b6bca94934f1d20335104ca28dea
Henry Paulino <henrypl360@gmail.com>
2019-02-21 19:06:48 -0500

    function

--- src/views/ImageDetails.js
+++ src/views/ImageDetails.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Image, Text } from 'react-native';
 import SearchContext from '../SearchContext';
 
-class ImageDetails extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, paddingVertical: 30, backgroundColor: '#000' }}>
-        <SearchContext.Consumer>
-          {({ selectedImage: {
-            webformatHeight,
-            webformatURL,
-            tags
-          }}) => (
-            <>
-              <Image
-                style={{ resizeMode: 'contain', height: webformatHeight }}
-                source={{ uri: webformatURL }}
-              />
-              <Text style={{ textAlign: 'center', color: '#fff' }}>
-                Tags: {tags}
-              </Text>
-            </>
-          )}
-        </SearchContext.Consumer>
-      </View>
-    )
-  }
-}
+const ImageDetails = () => (
+  <View style={{ flex: 1, paddingVertical: 30, backgroundColor: '#000' }}>
+    <SearchContext.Consumer>
+      {({ selectedImage: {
+        webformatHeight,
+        webformatURL,
+        tags
+      }}) => (
+        <>
+          <Image
+            style={{ resizeMode: 'contain', height: webformatHeight }}
+            source={{ uri: webformatURL }}
+          />
+          <Text style={{ textAlign: 'center', color: '#fff' }}>
+            Tags: {tags}
+          </Text>
+        </>
+      )}
+    </SearchContext.Consumer>
+  </View>
+);
 
 export default ImageDetails;
